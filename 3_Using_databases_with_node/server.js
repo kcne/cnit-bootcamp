32e0bc1490bde2c6825b95914709a1347f21692f
Emin Kocan <kocanmn.dev@gmail.com>
2024-12-24 19:03:12 +0100

    add cors

--- 3_Using_databases_with_node/server.js
+++ 3_Using_databases_with_node/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const itemRoutes = require('./routes/itemRoutes');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
+app.use(cors());
+
 
 // Routes
 app.use('/api/items', itemRoutes);
